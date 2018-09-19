6372aefb6beefca62052db4bd2662c4d601c05ad
nimawapt <purnioffic@gmail.com>
2018-09-20 00:54:06 +0530

    update vendor view table

--- src/GUI/UpdateVendor.java
+++ src/GUI/UpdateVendor.java
@@ -48,6 +48,12 @@ public class UpdateVendor extends javax.swing.JFrame {
     String url="jdbc:sqlserver://localhost:1433;databaseName=Viskam_Flora_DB";
     String user="mahen123";
     String pass="1234";
+    */
+    
+    String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    String url="jdbc:sqlserver://localhost:1433;databaseName=Viskam_Flora_DB";
+    String user="mahen123";
+    String pass="1234";
     ResultSet rs;
     
     
